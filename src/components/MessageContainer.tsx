f8cb37729133edb72cf9330a91c2a60a70b784b7
Stephen <StephenTangCook@gmail.com>
2024-06-19 22:16:59 -0700

    remove extra

--- src/components/MessageContainer.tsx
+++ src/components/MessageContainer.tsx
@@ -1,5 +1,30 @@
 'use client';
 
-const MessageContainer = () => <div>Message container</div>;
+import type { Block } from 'slack-blocks-to-jsx';
+import { Message } from 'slack-blocks-to-jsx';
+
+const MessageContainer = () => {
+  const blocks: Block[] = [
+    {
+      type: 'section',
+      text: {
+        type: 'mrkdwn',
+        text: 'This is a mrkdwn section block :ghost: *this is bold*, and ~this is crossed out~, and <https://google.com|this is a link>',
+      },
+    },
+  ];
+
+  return (
+    <div>
+      Message container
+      <Message
+        time={new Date()}
+        name="Your Company"
+        logo="logo_url"
+        blocks={blocks}
+      />
+    </div>
+  );
+};
 
 export { MessageContainer };
